1e214d1a2683f30d82f924a925385a4ba624aa79
dhall1 <dhall1@users.noreply.github.com>
2016-02-10 18:41:36 -0500

    Added Launchpad.hpp

--- src/launchpad.hpp
+++ src/launchpad.hpp
@@ -5,11 +5,38 @@
 
 class Launchpad {
 public:
-	// Switches
+	// Button variables may be renamed
+	// Lone switch
+	Button * const SWITCH;
+	
+	// Switches in 1x4 configuration
 	Button * const SWITCH_1;
-	Button * const SWITCH_1;
-	Button * const SWITCH_1;
-	Button * const SWITCH_1;
+	Button * const SWITCH_2;
+	Button * const SWITCH_3;
+	Button * const SWITCH_4;
+	
+	// Buttons in 3x3 configuration
+	Button * const BUTTON;
+	Button * const BUTTON_1;
+	Button * const BUTTON_2;
+	Button * const BUTTON_3;
+	Button * const BUTTON_4;
+	Button * const BUTTON_5;
+	Button * const BUTTON_6;
+	Button * const BUTTON_7;
+	Button * const BUTTON_8;
+	
+	// Buttons in 1x3 configuration
+	Button * const BUTTON_9;
+	Button * const BUTTON_10;
+	Button * const BUTTON_11;
+	
+	// Big green buttons
+	Button * const GREEN;
+	Button * const GREEN_1;
+	
+	// Big red button
+	Button * const RED;
 };
 
 
