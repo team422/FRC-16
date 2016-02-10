f0a4908e99df87707cf7768b6ff791bfec452b93
dhall1 <dhall1@users.noreply.github.com>
2016-02-10 18:29:24 -0500

    Create launchpad.hpp

---
+++ src/launchpad.hpp
@@ -0,0 +1,16 @@
+#ifndef LAUNCHPAD_HPP
+#define LAUNCHPAD_HPP
+
+#include <WPILib.h>
+
+class Launchpad {
+public:
+	// Switches
+	Button * const SWITCH_1;
+	Button * const SWITCH_1;
+	Button * const SWITCH_1;
+	Button * const SWITCH_1;
+};
+
+
+#endif // LAUNCHPAD_HPP
