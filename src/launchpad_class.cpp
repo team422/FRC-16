87e279ae7275142c13425948a8ecf92d4ff6bbd5
Mason Beahr <iwaitfornachos@gmail.com>
2016-02-10 16:32:23 -0800

    Create launchpad_class.cpp

---
+++ src/launchpad_class.cpp
@@ -0,0 +1,9 @@
+#include "launchpad_class.hpp"
+
+#include <WPILib.h>
+
+Launchpad::Launchpad(uint_32 port) :
+launchpad( new Launchpad(port) ),
+BIG_BUTTON_1( new JoystickButton(launchpad, X),
+BIG_BUTTON_1( new JoystickButton(launchpad, X),
+BIG_BUTTON_1( new JoystickButton(launchpad, X),
