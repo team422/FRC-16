9f8ea7254c021b1ee2e014f8e9e58e8a32007d1a
dhall1 <dhall1@users.noreply.github.com>
2016-02-11 08:43:26 -0500

    Update intake.hpp
    Added is_moving

--- src/subsystems/intake.hpp
+++ src/subsystems/intake.hpp
@@ -9,11 +9,11 @@ public:
 	void InitDefaultCommand();
 	void set_roller_normalized(float speed);
 	void set_arm_normalized(float speed);
-
+	bool is_moving;
 private:
 	CANTalon *roller,
-	         *arm_1,
-			 *arm_2;
+	        *arm_1,
+		*arm_2;
 };
 
 #endif // INTAKE_HPP
