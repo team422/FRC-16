9a9bb202e4ca0b743359b7fc381ebd53c2aa565b
dhall1 <dhall1@users.noreply.github.com>
2016-02-10 20:39:57 -0500

    Update launchpad.hpp
    Renamed buttons

--- src/launchpad.hpp
+++ src/launchpad.hpp
@@ -7,16 +7,16 @@ class Launchpad {
 public:
 	Launchpad(uint32_t port);
 private:
-	// The Launchpad
 	Joystick * launchpad;
 public:
 	// Some of the buttons are not connected to the launchpad,
-	// so they have been commented out
+	// but are left and commented to keep the buttons in order
 	
-	// Button variables may be renamed with more appropriate names
+	// Button variables may need to be renamed with more
+	// appropriate names
 	
 	// Lone switch
-	//Button * const SWITCH;
+	//Button * const LONE_SWITCH;
 	
 	// Row of 4 switches
 	//Button * const SWITCH_1;
@@ -24,8 +24,10 @@ public:
 	Button * const SWITCH_3;
 	Button * const SWITCH_4;
 	
-	// Buttons in 3x3
-	//Button * const BUTTON;
+	// Buttons in 3x3 square
+	// 1, 2, and 3 are in the top row
+	// 4, 5, and 6 are in the middle row
+	// 7, 8, and 9 are in the bottom row
 	Button * const BUTTON_1;
 	Button * const BUTTON_2;
 	Button * const BUTTON_3;
@@ -34,15 +36,16 @@ public:
 	Button * const BUTTON_6;
 	Button * const BUTTON_7;
 	Button * const BUTTON_8;
+	Button * const BUTTON_9;
 	
 	// Column of 3 buttons
-	//Button * const BUTTON_9;
-	//Button * const BUTTON_10;
-	//Button * const BUTTON_11;
+	//Button * const COLUMN_1;
+	//Button * const COLUMN_2;
+	//Button * const COLUMN_3;
 	
 	// Big green buttons
-	Button * const GREEN;
 	Button * const GREEN_1;
+	Button * const GREEN_2;
 	
 	// Big red button
 	//Button * const RED;
