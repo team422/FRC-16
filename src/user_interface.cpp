11548677d16ce1870ddc480fdd03c6ad82cd99e6
dhall1 <dhall1@users.noreply.github.com>
2016-02-10 20:46:46 -0500

    Update user_interface.cpp
    Changed launchpad to a Launchpad instead of a Joystick

--- src/user_interface.cpp
+++ src/user_interface.cpp
@@ -6,7 +6,7 @@
 Joystick *UI::Primary_Driver::left_stick = 0,
          *UI::Primary_Driver::right_stick = 0;
 
-Joystick *UI::Secondary_Driver::launchpad = 0;
+Launchpad *UI::Secondary_Driver::launchpad = 0;
 Xbox_Controller *UI::Secondary_Driver::controller = 0;
 
 float intake_speed = 0.5;
@@ -15,7 +15,7 @@ void UI::initialize() {
 	Primary_Driver::left_stick = new Joystick(1);
 	Primary_Driver::right_stick = new Joystick(2);
 
-	Secondary_Driver::launchpad = new Joystick(0);
+	Secondary_Driver::launchpad = new Launchpad(0);
 	Secondary_Driver::controller = new Xbox_Controller(3);
 	
 	Secondary_Driver::controller::A->WhileHeld(new Set_Intake_Speed(intake_speed));
