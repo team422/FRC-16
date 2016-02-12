79c14cb9df890f2e1a9fc77f63d4b35c9ff0e756
Eric <ecampbell565@gmail.com>
2016-02-11 20:12:02 -0500

    Update user_interface.cpp
    Removed unnecessary code, added launchpad object

--- src/user_interface.cpp
+++ src/user_interface.cpp
@@ -1,16 +1,14 @@
 #include "user_interface.hpp"
 #include "port_mapping.hpp"
 #include "commands/set_intake_speed.hpp"
+#include "launcher.hpp"
 #include <WPILib.h>
 
 Joystick *UI::Primary_Driver::left_stick = 0,
          *UI::Primary_Driver::right_stick = 0;
 
-Joystick *UI::Secondary_Driver::launchpad = 0;
+Launchpad *UI::Secondary_Driver::launchpad = 0;
 Xbox_Controller *UI::Secondary_Driver::controller = 0;
-JoystickButton *UI::Secondary_Driver::intake_1 = 0,
-               *UI::Secondary_Driver::intake_2 = 0,
-			   *UI::Secondary_Driver::intake_3 = 0;
 
 float intake_speed = 0.5;
 
@@ -18,14 +16,8 @@ void UI::initialize() {
 	Primary_Driver::left_stick = new Joystick(1);
 	Primary_Driver::right_stick = new Joystick(2);
 
-	Secondary_Driver::launchpad = new Joystick(0);
+	Secondary_Driver::launchpad = new Launchpad(0);
 	Secondary_Driver::controller = new Xbox_Controller(3);
-	Secondary_Driver::intake_1 = new JoystickButton(Primary_Driver::left_stick,
-													Ports::Launchpad::INTAKE_FORWARD);
-	Secondary_Driver::intake_2 = new JoystickButton(Primary_Driver::left_stick,
-	                                                Ports::Launchpad::INTAKE_BACKWARD);
-	Secondary_Driver::intake_3 = new JoystickButton(Primary_Driver::left_stick,
-	                                                Ports::Launchpad::INTAKE_STOP);
 	
 	Secondary_Driver::controller::A->WhileHeld( new Set_Intake_Speed(intake_speed) );
 	Secondary_Driver::controller::B->WhileHeld( new Set_Intake_Speed(-intake_speed) );
