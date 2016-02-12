3c764d95ad3ecfac344e2f2db1136ce241597441
Eric <ecampbell565@gmail.com>
2016-02-11 20:33:56 -0500

    Update intake.hpp

--- src/subsystems/intake.hpp
+++ src/subsystems/intake.hpp
@@ -9,7 +9,9 @@ public:
 	void InitDefaultCommand();
 	void set_roller_normalized(float speed);
 	void set_arm_normalized(float speed);
-	bool is_moving;
+	
+	//Used to determine the current movement of roller, -1 is backwards, 0 is off, 1 is forward
+	int move_direction;
 
 private:
 	CANTalon *roller,
