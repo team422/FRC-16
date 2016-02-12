4efb2a0c3adcdc4d7ae8261eed23950060d76eac
Eric <ecampbell565@gmail.com>
2016-02-11 20:47:54 -0500

    Update set_intake_speed.cpp
    Added functionality to prevent roller motor conflict

--- src/commands/set_intake_speed.cpp
+++ src/commands/set_intake_speed.cpp
@@ -8,10 +8,26 @@ speed(speed) {
 	Requires(Subsystems::intake);
 }
 
+//Checks the current move direction of the roller motor, and prevents motor conflicts
+//Since Commands aren't multithreaded, synchronization shouldn't be an issue
 void Set_Intake_Speed::Execute() {
-	Subsystems::intake->set_roller_normalized(speed);
+	if(speed < 0) {
+		Subsystems::intake->move_direction = -1;
+		if(Subsystems::intake->move_direction = 1) { } else {
+			Subsystems::intake->set_roller_normalized(speed);
+		}
+	} else if(speed > 0) {
+		Subsystems::intake->move_direction = 1;
+		if(Subsystems::intake->move_direction = -1) { } else {
+			Subsystems::intake->set_roller_normalized(speed);
+		}
+	}
 }
 
 bool Set_Intake_Speed::IsFinished() {
 	return false;
+}
+
+void Set_Intake_Speed:Interrupted() {
+	Subsystems::intake->move_direction = 0; //Reset move direction to unmoving
 }
