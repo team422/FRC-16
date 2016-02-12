b00f3ecbe123c731cf567e77bc56e106d36fb535
Eric <ecampbell565@gmail.com>
2016-02-11 20:32:09 -0500

    Update intake.cpp

--- src/subsystems/intake.cpp
+++ src/subsystems/intake.cpp
@@ -7,7 +7,8 @@ Intake::Intake() :
 Subsystem("Intake"),
 roller( new CANTalon(Ports::CANBusIDs::INTAKE_ROLLER) ),
 arm_1( new CANTalon(Ports::CANBusIDs::INTAKE_ARM_1) ),
-arm_2( new CANTalon(Ports::CANBusIDs::INTAKE_ARM_2) ) {
+arm_2( new CANTalon(Ports::CANBusIDs::INTAKE_ARM_2) ),
+is_moving(0) {
 }
 
 void Intake::set_roller_normalized(float speed) {
