d0f7c6554546dd5c9c4a29442eec0cb02ab9677e
Eric Campbell <ecampbell565@gmail.com>
2016-02-23 17:46:55 -0500

    Added launchpad class and functionality

--- src/user_interface.cpp
+++ src/user_interface.cpp
@@ -8,9 +8,7 @@
 Joystick *UI::Primary_Driver::left_stick = 0,
          *UI::Primary_Driver::right_stick = 0;
 
-Joystick *UI::Secondary_Driver::launchpad = 0;
-JoystickButton *UI::Secondary_Driver::intake_double = 0,
-			   *UI::Secondary_Driver::intake_zero_pid;
+Launchpad *UI::Secondary_Driver::launchpad = 0;
 
 Xbox_Controller *UI::Secondary_Driver::controller = 0;
 
@@ -20,14 +18,11 @@ void UI::initialize() {
 	Primary_Driver::left_stick = new Joystick(1);
 	Primary_Driver::right_stick = new Joystick(2);
 
-	Secondary_Driver::launchpad = new Joystick(0);
+	Secondary_Driver::launchpad = new Launchpad(0);
 	Secondary_Driver::controller = new Xbox_Controller(3);
-
-	Secondary_Driver::intake_double = new JoystickButton(Secondary_Driver::launchpad, 8);
-	Secondary_Driver::intake_zero_pid = new JoystickButton(Secondary_Driver::launchpad, 12);
 
 	Secondary_Driver::controller->B->WhileHeld( new Set_Intake_Speed(intake_speed) );
 	Secondary_Driver::controller->A->WhileHeld( new Set_Intake_Speed(-intake_speed) );
 	Secondary_Driver::controller->RIGHT_BUMPER->WhenPressed( new Fire() );
-	Secondary_Driver::intake_zero_pid->WhenPressed( new Zero_Intake_PID() );
+	Secondary_Driver::launchpad->INTAKE_ZERO_PID->WhenPressed( new Zero_Intake_PID() );
 }
